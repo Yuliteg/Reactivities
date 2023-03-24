f7360932c28d3393caa73248654b1893c9904279
Yulia <78503488+Yuliteg@users.noreply.github.com>
2023-03-24 11:00:34 +0100

    home-page

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,6 +1,7 @@
 import {RouterProvider} from 'react-router-dom'
 import ReactDOM from "react-dom/client";
 import "semantic-ui-css/semantic.min.css";
+import "react-calendar/dist/Calendar.css"
 import "./app/layout/styles.css";
 import reportWebVitals from "./reportWebVitals";
 import { store, StoreContext } from "./app/stores/store";
